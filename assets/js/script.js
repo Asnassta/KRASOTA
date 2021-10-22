dcbccbdb4a6e22b5d69d3560081ddd0b8c739127
Asnassta <79850172+Asnassta@users.noreply.github.com>
2021-10-22 11:48:10 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	/*======Menu-toggle=============*/
+	
+	$(".burger").on("click", function() {
+		$(this).toggleClass("active");
+		$(".header__nav").toggleClass('active');
+	});
+	/*==========/menu-toggle=========*/
+
+	
+});
